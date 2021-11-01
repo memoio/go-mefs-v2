59fc9868a640a186093334b98a63a9b42d23fd26
fjt <yydfjt@memo>
2021-11-01 19:15:26 +0800

    fix

--- lib/keeper/keeper.go
+++ lib/keeper/keeper.go
@@ -1,33 +1,14 @@
 package keeper
 
 import (
-	"github.com/memoio/go-mefs-v2/lib/nodeinfo"
-	mpb "github.com/memoio/go-mefs-v2/lib/pb"
+	"github.com/memoio/go-mefs-v2/lib/pb"
 	"github.com/memoio/go-mefs-v2/lib/types"
 )
 
 type LocalInfo struct {
-	role    mpb.NodeInfo_RoleType
+	role    pb.NodeInfo_RoleType
 	localID uint64
 	groupID uint64
-}
-
-// manage users
-type UserManger struct {
-	groupID uint64
-	users   map[uint64]*nodeinfo.NodeInfo
-}
-
-// manage keepers in this group
-type KeeperManger struct {
-	groupID uint64
-	keepers map[uint64]*nodeinfo.NodeInfo
-}
-
-// manage providers in this group
-type ProManger struct {
-	groupID uint64
-	pros    map[uint64]*nodeinfo.NodeInfo
 }
 
 // manage fs, challenege and pay
