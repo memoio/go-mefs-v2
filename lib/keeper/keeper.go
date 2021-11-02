ec27e5881693ac0d5292eebd75f5c988fc6f1e81
fjt <yydfjt@memo>
2021-11-02 13:18:03 +0800

    fix

--- lib/keeper/keeper.go
+++ lib/keeper/keeper.go
@@ -1,15 +1,8 @@
 package keeper
 
 import (
-	"github.com/memoio/go-mefs-v2/lib/pb"
 	"github.com/memoio/go-mefs-v2/lib/types"
 )
-
-type LocalInfo struct {
-	role    pb.NodeInfo_RoleType
-	localID uint64
-	groupID uint64
-}
 
 // manage fs, challenege and pay
 type dataManage struct {
