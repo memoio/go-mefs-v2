37632168cf78f747240b32c887d8c8941fbe73f4
fjt <yydfjt@memo>
2021-12-29 10:40:30 +0800

    fortest

--- build/common.go
+++ build/common.go
@@ -7,7 +7,7 @@ import (
 
 const (
 	DefaultSegSize      = 248 * 1024 // byte
-	DefaultChalDuration = 200        // slot
+	DefaultChalDuration = 30         // slot
 	SlotDuration        = 30         // seconds
 	OrderDuration       = 10 * 86400 // 1 days
 )
