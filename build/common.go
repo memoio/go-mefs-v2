f74d4739cd4d7b20a2e8c40eba70c725e42c0eed
fjt <yydfjt@memo>
2022-01-04 10:55:49 +0800

    fix

--- build/common.go
+++ build/common.go
@@ -6,12 +6,12 @@ import (
 )
 
 const (
-	DefaultSegSize      = 248 * 1024         // byte
-	DefaultChalDuration = 30                 // slot
-	SlotDuration        = 30                 // seconds
-	OrderDuration       = 10 * 86400         // 1 days
-	OrderMin            = OrderDuration      // min 100days
-	OrderMax            = 10 * OrderDuration // max 1000 days
+	DefaultSegSize      = 248 * 1024   // byte
+	DefaultChalDuration = 30           // slot
+	SlotDuration        = 30           // seconds
+	OrderDuration       = OrderMin     // 1 days
+	OrderMin            = 10 * 86400   // min 100days
+	OrderMax            = 1000 * 86400 // max 1000 days
 )
 
 var (
