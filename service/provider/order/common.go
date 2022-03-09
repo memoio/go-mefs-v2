05b796d22bb24773151d87da5e56c5f2ee819ad8
fjt <yydfjt@memo>
2022-03-09 15:47:03 +0800

    add size judgement at provder

--- service/provider/order/common.go
+++ service/provider/order/common.go
@@ -5,3 +5,8 @@ import (
 )
 
 var logger = logging.Logger("pro-order")
+
+const (
+	orderMaxSize = 100 * 24 * 3600 * 10 * 1024 * 1024 * 1024 // 100 day*10GB
+	seqMaxSize   = 1024 * 1024 * 1024                        // 1 GB
+)
