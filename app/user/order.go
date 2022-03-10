1b097ca1ce1868e2cecf4464b8198a8fbf803587
fjt <yydfjt@memo>
2022-03-10 11:47:10 +0800

    filter output joblist

--- app/user/order.go
+++ app/user/order.go
@@ -27,6 +27,13 @@ var OrderCmd = &cli.Command{
 var orderListJobCmd = &cli.Command{
 	Name:  "jobList",
 	Usage: "list jobs of all pros",
+	Flags: []cli.Flag{
+		&cli.BoolFlag{
+			Name:  "verbose",
+			Usage: "filter output",
+			Value: false,
+		},
+	},
 	Action: func(cctx *cli.Context) error {
 		repoDir := cctx.String(cmd.FlagNodeRepo)
 		addr, headers, err := client.GetMemoClientInfo(repoDir)
@@ -45,7 +52,12 @@ var orderListJobCmd = &cli.Command{
 			return err
 		}
 
+		verbose := cctx.Bool("verbose")
+
 		for _, oi := range ois {
+			if !verbose && oi.InStop {
+				continue
+			}
 			fmt.Printf("proID: %d, jobs: %d, order: %d %s %s, seq: %d %s, ready: %t, stop: %t, avail: %s\n", oi.ID, oi.Jobs, oi.Nonce, ansi.Color(oi.OrderState, "green"), time.Unix(int64(oi.OrderTime), 0).Format(utils.SHOWTIME), oi.SeqNum, ansi.Color(oi.SeqState, "green"), oi.Ready, oi.InStop, time.Unix(int64(oi.AvailTime), 0).Format(utils.SHOWTIME))
 		}
 
