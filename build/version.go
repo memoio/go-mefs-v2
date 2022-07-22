a29a2688044cb9371c36687fab3b860a5c796c0d
wy <wy@example.com>
2022-07-22 14:31:11 +0800

    update version data

--- build/version.go
+++ build/version.go
@@ -3,7 +3,7 @@ package build
 var CurrentCommit string
 
 // BuildVersion is the local build version, set by build system
-const BuildVersion = "2.3.0721"
+const BuildVersion = "2.3.0722"
 
 func UserVersion() string {
 	return BuildVersion + CurrentCommit
