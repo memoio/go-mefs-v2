0565397e5d20eb12b61b2224b9469eaa0c634260
wy <wy@wy.com>
2022-12-20 15:55:09 +0800

    update version to 2.6.2

--- build/version.go
+++ build/version.go
@@ -8,7 +8,7 @@ var CurrentCommit string
 const ApiVersion = 2
 
 // BuildVersion is the local build version, set by build system
-const BuildVersion = "2.6.1"
+const BuildVersion = "2.6.2"
 
 func UserVersion() string {
 	return BuildVersion + fmt.Sprintf("+api.%d", ApiVersion) + CurrentCommit
