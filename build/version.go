41cabcc2515477d259068eb49c5b2e1088d594f3
wy <wy@example.com>
2023-02-13 11:50:29 +0800

    version 2.6.4.230213

--- build/version.go
+++ build/version.go
@@ -8,7 +8,7 @@ var CurrentCommit string
 const ApiVersion = 2
 
 // BuildVersion is the local build version, set by build system
-const BuildVersion = "2.6.3.1001"
+const BuildVersion = "2.6.4.230213"
 
 func UserVersion() string {
 	return BuildVersion + fmt.Sprintf("+api.%d", ApiVersion) + CurrentCommit
